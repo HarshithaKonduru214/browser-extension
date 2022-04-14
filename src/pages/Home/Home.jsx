cde2a72f4355fc148b2d2ed2d89fefedd3fc87d4
HarshithaKonduru214 <harshitakonduru2244@gmail.com>
2022-04-14 11:28:34 +0530

    fix: useEffect dependancy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -37,7 +37,7 @@ export default function Home() {
 
   useEffect(() => {
     getUserLocation();
-  }, []);
+  });
 
   const success = (position) => {
     getWeather(position.coords.latitude, position.coords.longitude);
